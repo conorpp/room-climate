e76b6efe4ec2d47724a523bc0de6750e86ba149f
Conor <conorpp@vt.edu>
2014-02-27 14:27:31 -0500

    added express framework

--- webserver.js
+++ webserver.js
@@ -1,15 +1,31 @@
 
 
-// server
+// WiFly server
 
 var server = require('net');
 
 server.createServer(function (socket) {
+    
     console.log("connected");
     console.log('socket connected: '+new Date(), socket.address());
     socket.on('data', function (data) {
         console.log('got data: ',data.toString());
     });
     socket.end();
+    
 }).listen(8080);
+
+// Web Server
+var port = 1337;
+var express = require("express");
+
+var app = express();
+app.set("view options", {layout: false});
+app.use(express.static(__dirname + "/public")); //use static files in ROOT/public folder
+
+app.get("/", function(request, response){ //root dir
+    response.render("index.html");
+});
+
+app.listen(port);
  
