48983e1e2475063142c24f35270989854079bb01
Conor <conorpp@vt.edu>
2014-03-06 13:44:54 -0500

    added setttings example

---
+++ settings-example.js
@@ -0,0 +1,17 @@
+
+/*
+    Rename this file as settings.js to enable it
+    
+    git will ignore the file.
+    
+    We will use this file to safely have differences in environments
+    for collaborator
+    
+*/
+
+var settings = {
+    web_port:8000,
+    tcp_port:8080
+};
+
+module.exports = settings;
