21a54198fd11fb2b64984ed475204e6fd78506cd
Conor <conorpp@vt.edu>
2014-03-06 15:19:31 -0500

    set up dynamic content

--- webserver.js
+++ webserver.js
@@ -21,12 +21,34 @@ var port = settings.web_port;
 var express = require("express");
 
 var app = express();
-app.set("view options", {layout: false});
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/public');          // looks for .html in /public
+  app.set('view engine', 'hbs');                    // it will automatically look for .html
+  app.use(express.static(__dirname + '/public'));   // looks for .js and .css in /public
+});
+app.engine('html', require('hbs').__express);   // hbs will load dynamic values
+
 app.use(express.static(__dirname + "/public")); //use static files in ROOT/public folder
 
 app.get("/", function(request, response){ //root dir
-    response.render("index.html");
+    response.render("index", {title:'my dynamic page'});
 });
+
+app.get("/*", function(request, response){ //root dir
+    
+    // look at all the data in the request object!
+    //console.log(request);
+    
+    // grab the /url and remove the '/' character.
+    var page = request.url.replace('/','');
+    console.log('requested '+page+' ');
+    response.render("index", {title:'my dynamic page'});
+});
+
+
 
 app.listen(port);
 
